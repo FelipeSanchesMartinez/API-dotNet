8d7203098d17e9df2bba8e8991c6ef7d74f0db96
Felipe Sanches <124208959+FelipeSanchesMartinez@users.noreply.github.com>
2023-05-27 09:21:26 -0300

    Creadted Models

---
+++ ContactAPI/ContactAPI/Model/ContactModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ContactAPI.Model
+{
+    public class ContactModel: Entity
+    {
+        public string Name { get; set; }
+        public int Contact { get; set; }
+        public string emailAddress { get; set; }
+    }
+}
---
+++ ContactAPI/ContactAPI/Model/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace ContactAPI.Model
+{
+    public class Entity
+    {
+        public long Id { get; set; }
+        public DateTime CreatAt { get; set; }
+        public DateTime UpdatedAt { get; set; } 
+        public DateTime DeletedAt { get; set; }
+    }
+}
